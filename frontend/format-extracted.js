c0ad0240d6b1581f49312a038dc76a4a9146dfdb
shanmukesh kunjam <kunjamshanmukesh@gmail.com>
2026-07-07 23:16:42 +0530

    cleanup: remove all auto-scripts, forensic tools and output blobs; harden .gitignore

--- frontend/format-extracted.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const content = fs.readFileSync(path.join(__dirname, 'extracted-elite-card.js'), 'utf8');
-const formatted = content.replace(/\\n/g, '\n').replace(/\\"/g, '"');
-fs.writeFileSync(path.join(__dirname, 'extracted-elite-card-formatted.js'), formatted);
-console.log('Done!');
--- frontend/read-footer-details.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const content = fs.readFileSync(path.join(__dirname, 'recovered_return_jsx.js'), 'utf8');
-
-console.log(content.slice(348500, 365000));
